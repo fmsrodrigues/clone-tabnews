d1a38c0274fd66579cfcedf0498a79ebf04ae54f
Phobia <filipe_msr@hotmail.com>
2023-11-07 17:49:46 -0300

    add `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,9 @@
-export default function (req, res) {
+import database from "../../../../infra/database"
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum;")
+  console.log(result.rows)
   return res.status(200).send({ message: "Running..." })
 }
+
+export default status
